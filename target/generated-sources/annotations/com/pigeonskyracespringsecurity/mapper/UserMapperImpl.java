8c892ebfe55c6291ce403acfd4ded743b3ed1837
kholoud001 <kholoud.sanak@gmail.com>
2024-12-18 12:02:07 +0100

    DEP-23 docker setting

--- target/generated-sources/annotations/com/pigeonskyracespringsecurity/mapper/UserMapperImpl.java
+++ target/generated-sources/annotations/com/pigeonskyracespringsecurity/mapper/UserMapperImpl.java
@@ -13,7 +13,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2024-12-06T14:02:42+0100",
+    date = "2024-12-18T09:44:10+0100",
     comments = "version: 1.5.3.Final, compiler: javac, environment: Java 17.0.12 (Oracle Corporation)"
 )
 @Component
