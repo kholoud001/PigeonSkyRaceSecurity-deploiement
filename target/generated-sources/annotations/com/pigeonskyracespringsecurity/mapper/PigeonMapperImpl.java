72b954f36471bd87d826b26c432b45b429c9933a
kholoud001 <kholoud.sanak@gmail.com>
2025-01-06 11:50:59 +0100

    new update for api cusmption

--- target/generated-sources/annotations/com/pigeonskyracespringsecurity/mapper/PigeonMapperImpl.java
+++ target/generated-sources/annotations/com/pigeonskyracespringsecurity/mapper/PigeonMapperImpl.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2024-12-19T13:20:34+0100",
+    date = "2024-12-26T14:34:47+0100",
     comments = "version: 1.5.3.Final, compiler: javac, environment: Java 17.0.12 (Oracle Corporation)"
 )
 @Component
